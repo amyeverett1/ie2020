4ed1ce856fa3700613abcd16413cfcac7b23bb0f
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-02-15 18:40:56 +0000

    fix about the day

--- about_the_day.php
+++ about_the_day.php
@@ -11,104 +11,42 @@
 
         
 
-<div class="container about the day">
-     
-
-<div class="wrapper">
-            
-
-<div class="box white">
-       
-                       
-                
-
-
-</div> <!-- psurple-->
-        
-</div> <!-- wrapper-->
-       
-     
-            
-   </div><!-- psurple-->
-        </div> <!-- wrapper-->  
-            
-              <!-- container-->
-<?php include('footer.php')?>
-
-
-
-
-<div class="wrapper">
- 
-<div class="box white">
-       
-                
- 
-<h2>What is the IE 2020 Advocates Network?</h2>
-<p>It’s a group of people from across IE who are interested in how and why IE is changing.  Network members want to find out more about IE 2020 and share this information with their colleagues.  Importantly, network members will help those leading IE’s change programmes to understand the impact of changes on teams across our organisation, by feeding back their own and colleagues’ views, concerns and suggestions.</p>
-  
-
-
- 
-</div> <!-- psurple-->
-        
-</div> <!-- wrapper-->
- 
- 
-<div class="wrapper">
- 
-<div class="box white">
-       
-<h2>What’s this event all about?</h2>
-<p>The aim of this event is to provide you with a basic understanding of how IE is changing, right across the business.  You may already be familiar with the changes going on in your area.  This is an opportunity to think bigger, to understand how the whole of IE is changing and to think about the impact of this on your own team’s work.</p>      
-
-
-
- 
-</div> <!-- psurple-->
-        
-</div> <!-- wrapper-->
- 
- 
-<div class="wrapper">
- 
-<div class="box white">
-<h2>Why have I been invited?</h2>
-<p>You have been identified as someone who is able to engage confidently with colleagues, take on board new information and discuss the challenges associated with IE 2020 in a professional, constructive way.  After the event, you will be expected to share what you have learned with your colleagues in a way that works well for your area of work.  There is no standard approach.  Instead, you will be provided with the information you require and contacts for any follow-up questions.  It’s up to you how you share what you’ve learned, start conversations and feed back views to those leading IE’s change programmes.  Don’t be afraid to innovate and try new ways of discussing IE 2020, focussing on the changes most relevant to your team’s work.</p>      
-
-
- 
-</div> <!-- psurple-->
-        
-</div> <!-- wrapper-->
- 
- 
-<div class="wrapper">
- 
-<div class="box white">                  
- 
-<h2>What else do I need to know?</h2>
-<p>Please wear smart business attire to the event, not uniforms.</p>
-<p>If you have any specific requirements, please let us know when booking.</p>
-<p>Lunch will be provided.</p>
-<p>Travel and accommodation, if required, must be booked through Redfern with costs met by your own business area.</p>      
-
- 
- 
-</div> <!-- psurple-->
-        
-</div> <!-- wrapper-->
-
+<div class="container">
     
-     
-     
-            
-   </div><!-- psurple-->
-        </div> <!-- wrapper-->  
-            
-              <!-- container-->
-        <footer>
-            <p>Created by <a href="https://twitter.com/hodigital">Home Office Digital</a> for <a href="https://www.gov.uk/government/organisations/immigration-enforcement">Immigration Enforcement</a></p>
-        </footer>
-    </body>
-</html>
+    <div class="wrapper">
+        <div class="box white">
+            <h2>What is the IE 2020 Advocates Network?</h2>
+            <p>It’s a group of people from across IE who are interested in how and why IE is changing.  Network members want to find out more about IE 2020 and share this information with their colleagues.  Importantly, network members will help those leading IE’s change programmes to understand the impact of changes on teams across our organisation, by feeding back their own and colleagues’ views, concerns and suggestions.</p>
+        </div> <!-- white-->
+    </div> <!-- wrapper-->
+
+ 
+    <div class="wrapper">
+        <div class="box white">
+            <h2>What’s this event all about?</h2>
+            <p>The aim of this event is to provide you with a basic understanding of how IE is changing, right across the business.  You may already be familiar with the changes going on in your area.  This is an opportunity to think bigger, to understand how the whole of IE is changing and to think about the impact of this on your own team’s work.</p>      
+        </div> <!-- psurple-->
+    </div> <!-- wrapper-->
+ 
+ 
+<div class="wrapper">
+    <div class="box white">
+        <h2>Why have I been invited?</h2>
+        <p>You have been identified as someone who is able to engage confidently with colleagues, take on board new information and discuss the challenges associated with IE 2020 in a professional, constructive way.  After the event, you will be expected to share what you have learned with your colleagues in a way that works well for your area of work.  There is no standard approach.  Instead, you will be provided with the information you require and contacts for any follow-up questions.  It’s up to you how you share what you’ve learned, start conversations and feed back views to those leading IE’s change programmes.  Don’t be afraid to innovate and try new ways of discussing IE 2020, focussing on the changes most relevant to your team’s work.</p>  
+    </div> <!-- psurple-->        
+</div> <!-- wrapper-->
+ 
+ 
+<div class="wrapper">
+    <div class="box white">                  
+        <h2>What else do I need to know?</h2>
+        <p>Please wear smart business attire to the event, not uniforms.</p>
+        <p>If you have any specific requirements, please let us know when booking.</p>
+        <p>Lunch will be provided.</p>
+        <p>Travel and accommodation, if required, must be booked through Redfern with costs met by your own business area.</p>     
+    </div> <!-- psurple-->
+</div> <!-- wrapper-->
+
+</div> <!-- container-->
+
+<?php include('footer.php')?>
